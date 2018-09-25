c2e72bc8543039adec53a8325cd770cf561f0d37
Adam Staněk <adam.stanek@ataccama.com>
2018-09-25 15:49:14 +0200

    lens

--- lib/lens.ts
+++ lib/lens.ts
@@ -1,30 +1,30 @@
 export interface PropertyLens<T> {
-  get(): T;
-  set(newValue: T): void;
+  get(): T
+  set(newValue: T): void
 }
 
 export type ObjectLens<T> = {
   [k in keyof T]-?: Extract<T[k], object> extends never
     ? PropertyLens<T[k]>
     : Lens<Extract<T[k], object>> & PropertyLens<T[k]>
-};
+}
 
 export interface ArrayLens<T> {
-  push(newItem: T): void;
-  [index: number]: T;
+  push(newItem: T): void
+  [index: number]: T
 }
 
 export type Lens<T> = T extends any[]
   ? ArrayLens<T[number]>
-  : T extends object ? ObjectLens<T> : T;
+  : T extends object ? ObjectLens<T> : T
 
-export type LensCallback<T, R> = (_: Lens<T>) => R;
+export type LensCallback<T, R> = (_: Lens<T>) => R
 export type LensReturnType<T, R> = R extends void
   ? T
-  : R extends PropertyLens<infer RR> ? RR : R;
+  : R extends PropertyLens<infer RR> ? RR : R
 
 export function lens<T extends object, R>(
-  callback: LensCallback<T, R>
+  callback: LensCallback<T, R>,
 ): LensReturnType<T, R> {
-  throw new Error("Not implemented yet");
+  throw new Error('Not implemented yet')
 }
