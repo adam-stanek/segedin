f36d9fb01d88951f139b11d49297a1a553d04626
Adam Staněk <adam.stanek@ataccama.com>
2018-11-12 15:37:48 +0100

    createSetForAccessorChain moved to separate file

--- lib/set.ts
+++ lib/set.ts
@@ -1,57 +1,5 @@
-import { isEqual as objDeepEqual } from 'lodash'
+import { createSetForAccessorChain } from './createSetForAccessorChain'
 import { retrieveAccessorChain } from './accessor'
-
-const createSetForAccessorChain = <T, R>(
-  root: R,
-  accessors: Array<string | symbol>,
-) => (value: T | ((_: T, root: R) => T)): R => {
-  const currentNode: any = root
-
-  if (accessors.length === 0) {
-    // currentNode is the target
-    const newNode: any =
-      value instanceof Function ? value(currentNode, root) : value
-
-    // Return currentNode if structural equality
-    return objDeepEqual(currentNode, newNode) ? currentNode : newNode
-  } else {
-    // currentNode is a parent of the target
-    const [key, ...nextAccessors] = accessors
-    const newValue = createSetForAccessorChain(
-      currentNode && currentNode[key],
-      nextAccessors,
-    )(value)
-
-    if (currentNode) {
-      // Return currentNode if new value is identical
-      if (currentNode[key] === newValue) {
-        return currentNode
-      } else if (Array.isArray(currentNode)) {
-        const copy = currentNode.slice()
-        copy[key] = newValue
-        return copy as any
-      } else {
-        return Object.assign(
-          Object.create(Object.getPrototypeOf(currentNode)),
-          currentNode,
-          {
-            [key]: newValue,
-          },
-        )
-      }
-    } else {
-      if (typeof key === 'number') {
-        const a = []
-        a[key] = newValue
-        return a as any
-      } else {
-        return {
-          [key]: newValue,
-        } as any
-      }
-    }
-  }
-}
 
 export function set<R extends object, T>(
   root: R,
